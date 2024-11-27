5b9bbd7b9617d19921886446598b21014971ece0
HLF <lamlethanh987@gmail.com>
2024-11-27 17:21:13 +0700

    fix source

--- E-Commerce_Web_Application/User/server.js
+++ E-Commerce_Web_Application/User/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 require('dotenv').config();
 require('./config/db_conn');
-const port = process.env.PORT || 9001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
